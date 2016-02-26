161c0afc187732d8249e429150c20bde4edcb006
vivian <vivian.allum@gmail.com>
2016-02-26 14:46:57 -0500

    -working basic enemy

--- Assets/Scripts/Character Scripts/EnemyRoamer.cs
+++ Assets/Scripts/Character Scripts/EnemyRoamer.cs
@@ -4,10 +4,17 @@ using System.Collections;
 public class EnemyRoamer : MonoBehaviour {
 	// This Enemy roams around, pursues player in a radius, and has a melee weapon.
 
-	bool changeStatus = true; 
-	string state = "none";
+	bool changeStatus = false; 
+//	string state = "none";
+	enum state { none, Pursue, Wander };
+	state enemyState = state.none;
+	float xValue = 0;
+	float yValue = 0;
+	public float speed = 0.5f;
 
 	void Start () {
+		 xValue = Random.Range(-1f,1f);
+		 yValue = Random.Range(-1f,1f);
 
 	}
 
@@ -16,21 +23,45 @@ public class EnemyRoamer : MonoBehaviour {
 		CheckStatus ();
 
 		if (changeStatus) {
-			this.transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z); 
+			switch (enemyState) {
+			case state.Pursue:
+				PursuePlayer ();
+				changeStatus = false;
+				break;
+			default:
+				break;
+			}
+		} else {
+			Wander ();
 		}
+
 	}
 
 	void CheckStatus(){
 		// Check what action to do next
-		if (
+		if (CheckForPlayer ()) {
+			if (enemyState != state.Pursue) {
+				changeStatus = true; 
+				enemyState = state.Pursue;
+			}
+		} else {
+			if (enemyState != state.Wander) {
+				enemyState = state.Wander;
+			}
+		}
 	}
 
 	void Wander(){
+		Debug.Log ("wandeR" + xValue);
+//		this.transform.position = new Vector3 (transform.position.x+ Random.Range(-1f,1f), transform.position.y+ Random.Range(-1f,1f), transform.position.z); 
+		this.transform.position = new Vector3 (transform.position.x + xValue *speed, transform.position.y+ yValue *speed, transform.position.z); 
 
+		//transform.position += moveDirection;
 
 	}
 
-	void CheckForPlayer(){
+	bool CheckForPlayer(){
+		return false; // temp
 		
 	}
 
