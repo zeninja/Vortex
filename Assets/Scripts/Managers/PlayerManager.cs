6daa74b8dc01004a82bf68ca366d37024bf5381c
Adnan Agha <aea313@nyu.edu>
2016-02-26 17:29:26 -0500

    - Started working on multitouch

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -11,18 +11,13 @@ public class PlayerManager : MonoBehaviour {
 	PolygonCollider2D polygonCollider;
 	LineRenderer[] lineRenderers = new LineRenderer[3];
 	
-	float[] currentSlopes;
-	float[] prevSlopes;
-	float[] comparedSlopes;
-	
-	float[] changingSlopes;
-	
 	List<GameObject> collidingObjects = new List<GameObject>();
+	
+	public Vector2[] startingPositions = new Vector2[3];
+	public Vector2[] currentPositions  = new Vector2[3];
 	
 	public GameObject graphics;
 	
-//	Vector2[] comparedSlopes;
-
 	// Use this for initialization
 	void Start () {
 		polygonCollider = GetComponent<PolygonCollider2D>();
@@ -31,20 +26,29 @@ public class PlayerManager : MonoBehaviour {
 			characters[i].GetComponent<Character>().playerManager = this;
 			lineRenderers[i] = characters[i].GetComponent<LineRenderer>();
 		}
-		
-		currentSlopes  = new float[3];
-		prevSlopes 	   = new float[3];
-		
-		comparedSlopes = new float[3];
-		
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		UpdateCollider();
 		UpdateLineDisplay();
-		CalculateSlopesBetweenCharacters();
+		UpdateInputs();
+	}
+	
+	void UpdateInputs() {
+		
+	}
+	
+	void OnFingerDown(FingerDownEvent e) {
+		startingPositions[e.Finger.Index] = e.Position;
+	}
+	
+	void OnFingerMove(FingerMotionEvent e) {
+		currentPositions[e.Finger.Index] = e.Position;
+	}
+	
+	void OnFingerUp(FingerUpEvent e) {
+		
 	}
 	
 	public void SetCharacterType(CharacterType latestCharacter) {
@@ -72,52 +76,6 @@ public class PlayerManager : MonoBehaviour {
 		}
 		
 		polygonCollider.SetPath(0, colliderPath);
-	}
-	
-	void CalculateSlopesBetweenCharacters() {
-		currentSlopes[0] = (characters[1].transform.position.y - characters[0].transform.position.y)/(characters[1].transform.position.x - characters[0].transform.position.x);
-		currentSlopes[1] = (characters[2].transform.position.y - characters[1].transform.position.y)/(characters[2].transform.position.x - characters[1].transform.position.x);
-	    currentSlopes[2] = (characters[0].transform.position.y - characters[2].transform.position.y)/(characters[0].transform.position.x - characters[2].transform.position.x);
-		
-//		int numSlopesChanged = 0;
-//		
-//		for (int i = 0; i < 3; i++) {
-//			if (currentSlopes[i] != prevSlopes[i]) {
-//				numSlopesChanged++;
-//			}
-//		}
-//		
-//		if(numSlopesChanged == 2) {
-//			Debug.Log("Crossed over!!!");
-//		}
-
-//		comparedSlopes[0].x = currentSlopes[0] - currentSlopes[1];
-//		comparedSlopes[0].y = currentSlopes[0] - currentSlopes[2];
-//		
-//		comparedSlopes[1].x = currentSlopes[1] - currentSlopes[0];
-//		comparedSlopes[1].y = currentSlopes[1] - currentSlopes[2];
-//		
-//		comparedSlopes[2].x = currentSlopes[2] - currentSlopes[0];
-//		comparedSlopes[2].y = currentSlopes[2] - currentSlopes[1]
-
-//		prevSlopes[0] = (characters[1].transform.position.y - characters[0].transform.position.y)/(characters[1].transform.position.x - characters[0].transform.position.x);
-//		prevSlopes[1] = (characters[2].transform.position.y - characters[1].transform.position.y)/(characters[2].transform.position.x - characters[1].transform.position.x);
-//		prevSlopes[2] = (characters[0].transform.position.y - characters[2].transform.position.y)/(characters[0].transform.position.x - characters[2].transform.position.x);
-
-//		int movingIndex = 0;
-//		
-//		for(int i = 0; i < 3; i++) {
-//			if(characters[i].GetComponent<Character>().moving) {
-//				movingIndex = i;
-//			}
-//		}
-//		
-//		if (movingIndex == 0) {
-//			changingSlopes[0] = currentSlopes[0];
-//			changingSlopes[1] = currentSlopes[1];
-//		} else if (movingIndex == 0) {
-//			
-//		}
 	}
 	
 	void OnTap (TapGesture gesture) {			
