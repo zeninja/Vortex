6ca2d0b9adec1dc19cfa685c3d323dafe98046b8
vivian <vivian.allum@gmail.com>
2016-02-29 13:19:29 -0500

    - enemy spawning

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	public GameObject enemy;
 	// Use this for initialization
 	void Start () {
-	
+		InvokeRepeating("EnemySpawner",0,3);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void EnemySpawner(){
+		int randomColor = Random.Range (0, 3);
+		Debug.Log (randomColor);
+		if (randomColor == 0) {
+			enemy.GetComponent<EnemyRoamer> ().enemyType = PlayerManager.CharacterType.Red;
+			enemy. GetComponentInChildren<SpriteRenderer> ().color = Color.red;
+		} else if (randomColor == 1) {
+			enemy.GetComponent<EnemyRoamer> ().enemyType = PlayerManager.CharacterType.Blue;
+			enemy.GetComponentInChildren<SpriteRenderer> ().color = Color.blue;
+		} else {
+			enemy.GetComponent<EnemyRoamer> ().enemyType = PlayerManager.CharacterType.Yellow;
+			enemy.GetComponentInChildren<SpriteRenderer> ().color = Color.yellow;
+
+
+		}
+		Object newEnemy = Instantiate(enemy, new Vector3(Random.Range (-30, 30), Random.Range (-25, 25), 0),
+			Quaternion.identity); //fix spawn locations
+		
+
 	}
 }
