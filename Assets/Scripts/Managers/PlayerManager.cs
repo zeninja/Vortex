2096fa3d56c67c041cbec657458c4c20a9027a02
Adnan Agha <aea313@nyu.edu>
2016-02-29 17:59:13 -0500

    - Added multitouch and trigger on release instead of tap

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -11,17 +11,13 @@ public class PlayerManager : MonoBehaviour {
 	PolygonCollider2D polygonCollider;
 	LineRenderer[] lineRenderers = new LineRenderer[3];
 	
-	float[] currentSlopes;
-	float[] prevSlopes;
-	float[] comparedSlopes;
-	
-	float[] changingSlopes;
-	
 	List<GameObject> collidingObjects = new List<GameObject>();
 	
 	public GameObject graphics;
 	int originalNumObjects = 0;
-//	Vector2[] comparedSlopes;
+
+//	[System.NonSerialized]
+	public Vector2[] offsets = new Vector2[3];
 
 	// Use this for initialization
 	void Start () {
@@ -31,20 +27,12 @@ public class PlayerManager : MonoBehaviour {
 			characters[i].GetComponent<Character>().playerManager = this;
 			lineRenderers[i] = characters[i].GetComponent<LineRenderer>();
 		}
-		
-		currentSlopes  = new float[3];
-		prevSlopes 	   = new float[3];
-		
-		comparedSlopes = new float[3];
-		
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		UpdateCollider();
 		UpdateLineDisplay();
-		CalculateSlopesBetweenCharacters();
 	}
 	
 	public void SetCharacterType(CharacterType latestCharacter) {
@@ -74,61 +62,35 @@ public class PlayerManager : MonoBehaviour {
 		polygonCollider.SetPath(0, colliderPath);
 	}
 	
-	void CalculateSlopesBetweenCharacters() {
-		currentSlopes[0] = (characters[1].transform.position.y - characters[0].transform.position.y)/(characters[1].transform.position.x - characters[0].transform.position.x);
-		currentSlopes[1] = (characters[2].transform.position.y - characters[1].transform.position.y)/(characters[2].transform.position.x - characters[1].transform.position.x);
-	    currentSlopes[2] = (characters[0].transform.position.y - characters[2].transform.position.y)/(characters[0].transform.position.x - characters[2].transform.position.x);
-		
-//		int numSlopesChanged = 0;
+	void OnTap (TapGesture gesture) {			
+//		Vector2 startPosition = Camera.main.ScreenToWorldPoint(gesture.StartPosition);		
+//		RaycastHit2D hit = Physics2D.Raycast(startPosition, Vector2.zero);
 //		
-//		for (int i = 0; i < 3; i++) {
-//			if (currentSlopes[i] != prevSlopes[i]) {
-//				numSlopesChanged++;
+//		if (hit.collider != null) {
+//			if(hit.collider == polygonCollider) {
+//				Trigger();
 //			}
-//		}
-//		
-//		if(numSlopesChanged == 2) {
-//			Debug.Log("Crossed over!!!");
-//		}
-
-//		comparedSlopes[0].x = currentSlopes[0] - currentSlopes[1];
-//		comparedSlopes[0].y = currentSlopes[0] - currentSlopes[2];
-//		
-//		comparedSlopes[1].x = currentSlopes[1] - currentSlopes[0];
-//		comparedSlopes[1].y = currentSlopes[1] - currentSlopes[2];
-//		
-//		comparedSlopes[2].x = currentSlopes[2] - currentSlopes[0];
-//		comparedSlopes[2].y = currentSlopes[2] - currentSlopes[1]
-
-//		prevSlopes[0] = (characters[1].transform.position.y - characters[0].transform.position.y)/(characters[1].transform.position.x - characters[0].transform.position.x);
-//		prevSlopes[1] = (characters[2].transform.position.y - characters[1].transform.position.y)/(characters[2].transform.position.x - characters[1].transform.position.x);
-//		prevSlopes[2] = (characters[0].transform.position.y - characters[2].transform.position.y)/(characters[0].transform.position.x - characters[2].transform.position.x);
-
-//		int movingIndex = 0;
-//		
-//		for(int i = 0; i < 3; i++) {
-//			if(characters[i].GetComponent<Character>().moving) {
-//				movingIndex = i;
-//			}
-//		}
-//		
-//		if (movingIndex == 0) {
-//			changingSlopes[0] = currentSlopes[0];
-//			changingSlopes[1] = currentSlopes[1];
-//		} else if (movingIndex == 0) {
-//			
 //		}
 	}
 	
-	void OnTap (TapGesture gesture) {			
-		Vector2 startPosition = Camera.main.ScreenToWorldPoint(gesture.StartPosition);
-//		int colliderLayer = 1 << LayerMask.NameToLayer("Player");
-		
-		RaycastHit2D hit = Physics2D.Raycast(startPosition, Vector2.zero);
-		
-		if (hit.collider != null) {
-			if(hit.collider == polygonCollider) {
+	void OnFingerDown(FingerDownEvent e) {
+		for(int i = 0; i < characters.Length; i++) {
+			if(e.Selection == characters[i]) {
+				characters[i].GetComponent<Character>().fingerIndex = e.Finger.Index;
+				offsets[e.Finger.Index] = (Vector2)characters[i].transform.position - (Vector2)Camera.main.ScreenToWorldPoint(e.Finger.Position);
+				//currentCharacter = characters[i].GetComponent<Character>().myCharacter;
+				SetCharacterType(characters[i].GetComponent<Character>().myCharacter);
+			}
+		}
+	}
+	
+	void OnFingerUp(FingerUpEvent e) {
+		for (int i = 0; i < characters.Length; i++) {
+			if (characters[i].GetComponent<Character>().fingerIndex == e.Finger.Index) {
+				SetCharacterType(characters[i].GetComponent<Character>().myCharacter);
 				Trigger();
+				
+				characters[i].GetComponent<Character>().fingerIndex = -1;
 			}
 		}
 	}
@@ -164,7 +126,6 @@ public class PlayerManager : MonoBehaviour {
 
 		}
 	}
-
 
 	void OnDrawGizmos() {
 		Gizmos.DrawLine(characters[0].transform.position, characters[1].transform.position);
