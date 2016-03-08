50f4397dd644dadf43168f917eae84957ba0ce39
vivian <vivian.allum@gmail.com>
2016-03-08 04:22:35 -0500

    no message

--- Assets/Scripts/Character Scripts/Character.cs
+++ Assets/Scripts/Character Scripts/Character.cs
@@ -13,9 +13,6 @@ public class Character : MonoBehaviour {
 	
 	Vector2 positionOffset;
 	
-	[System.NonSerialized]
-	public int fingerIndex = -1;
-	
 	// Use this for initialization
 	void Start () {
 		myCollider = transform.GetComponentInChildren<Collider2D>();
@@ -25,89 +22,85 @@ public class Character : MonoBehaviour {
 	void Update () {
 //		transform.position += playerManager[ 
 //		playerManager.inputs[
-		
-		if(fingerIndex >= 0) {
-			transform.position = (Vector2)Camera.main.ScreenToWorldPoint(FingerGestures.GetFinger(fingerIndex).Position) + playerManager.offsets[fingerIndex];
-		}
 	}
 	
-//	void OnFingerDown(FingerDownEvent fingerDown) {
-//		
+	void OnFingerDown(FingerDownEvent fingerDown) {
+		
 //		Debug.Log(fingerDown.Selection);
-//		
-//		// THIS IS PROBABLY A LITTLE BIT TOO INACCURATE FOR WHAT WE ACTUALLY NEED
-//	
-//		if(fingerDown.Selection == gameObject) {
-////			canMove = true;
-//			playerManager.SetCharacterType(myCharacter);
-//			positionOffset = (Vector2)transform.position - (Vector2)Camera.main.ScreenToWorldPoint(fingerDown.Position);
-//		}
-//
-//		//		if (gesture.Phase == ContinuousGesturePhase.Started) {
-//		//			Vector2 startPosition = Camera.main.ScreenToWorldPoint(gesture.StartPosition);
-//		//			int characterLayer = 1 << LayerMask.NameToLayer("Characters");
-//		//						
-//		//			RaycastHit2D hit = Physics2D.Raycast(startPosition, Vector2.zero, 0, characterLayer);
-//		//			
-//		//			if (hit.collider != null) {
-//		//				if(hit.collider == myCollider) {
-//		//					playerManager.SetCharacterType(myCharacter);
-//		//					
-//		//					canMove = true;
-//		//					positionOffset = startPosition - (Vector2)transform.position;
-//		//				}
-//		//			}
-//		//		}
-//	}
-//	
-//	void OnFingerMove(FingerMotionEvent fingerMove) {
-//	
-//////		if(fingerMove.Selection == gameObject) {
-////			if(canMove) {
-////			
-//////				if(fingerMove.Finger.Index) {
-//////					
-//////				}
-////				transform.position = (Vector2)Camera.main.ScreenToWorldPoint(fingerMove.Position) + positionOffset;
-////			}
-//////		}
-//	}
-//	
-//	void OnFingerUp(FingerUpEvent fingerUp) {
-//		if (fingerUp.Selection == gameObject) {
-////			canMove = false;	
-//		}
-//
-////		moving = false;
-//	}
-//	
-//	void OnDrag(DragGesture gesture) {
-//		Vector2 positionOffset = new Vector2();
-//		
-//		if (gesture.Phase == ContinuousGesturePhase.Started) {
-//			Vector2 startPosition = Camera.main.ScreenToWorldPoint(gesture.StartPosition);
-//			int characterLayer = 1 << LayerMask.NameToLayer("Characters");
-//						
-//			RaycastHit2D hit = Physics2D.Raycast(startPosition, Vector2.zero, 0, characterLayer);
+		
+		// THIS IS PROBABLY A LITTLE BIT TOO INACCURATE FOR WHAT WE ACTUALLY NEED
+	
+		if(fingerDown.Selection == gameObject) {
+//			canMove = true;
+			playerManager.SetCharacterType(myCharacter);
+			positionOffset = (Vector2)transform.position - (Vector2)Camera.main.ScreenToWorldPoint(fingerDown.Position);
+		}
+
+		//		if (gesture.Phase == ContinuousGesturePhase.Started) {
+		//			Vector2 startPosition = Camera.main.ScreenToWorldPoint(gesture.StartPosition);
+		//			int characterLayer = 1 << LayerMask.NameToLayer("Characters");
+		//						
+		//			RaycastHit2D hit = Physics2D.Raycast(startPosition, Vector2.zero, 0, characterLayer);
+		//			
+		//			if (hit.collider != null) {
+		//				if(hit.collider == myCollider) {
+		//					playerManager.SetCharacterType(myCharacter);
+		//					
+		//					canMove = true;
+		//					positionOffset = startPosition - (Vector2)transform.position;
+		//				}
+		//			}
+		//		}
+	}
+	
+	void OnFingerMove(FingerMotionEvent fingerMove) {
+	
+////		if(fingerMove.Selection == gameObject) {
+//			if(canMove) {
 //			
-//			if (hit.collider != null) {
-//				if(hit.collider == myCollider) {
-//					playerManager.SetCharacterType(myCharacter);
-//					
-//					canMove = true;
-//					positionOffset = startPosition - (Vector2)transform.position;
-//				}
+////				if(fingerMove.Finger.Index) {
+////					
+////				}
+//				transform.position = (Vector2)Camera.main.ScreenToWorldPoint(fingerMove.Position) + positionOffset;
 //			}
-//		}
-//		
-//		if(gesture.Phase == ContinuousGesturePhase.Updated && canMove) {
-//			transform.position = (Vector2)Camera.main.ScreenToWorldPoint(gesture.Position) + positionOffset;
-//			moving = true;
-//		}
-//		
-//		if(gesture.Phase == ContinuousGesturePhase.Ended) {
-//			canMove = false;
-//			moving = false;
-//		}
-//	}
+////		}
+	}
+	
+	void OnFingerUp(FingerUpEvent fingerUp) {
+		if (fingerUp.Selection == gameObject) {
+//			canMove = false;	
+		}
+
+//		moving = false;
+	}
+	
+	void OnDrag(DragGesture gesture) {
+		Vector2 positionOffset = new Vector2();
+		
+		if (gesture.Phase == ContinuousGesturePhase.Started) {
+			Vector2 startPosition = Camera.main.ScreenToWorldPoint(gesture.StartPosition);
+			int characterLayer = 1 << LayerMask.NameToLayer("Characters");
+						
+			RaycastHit2D hit = Physics2D.Raycast(startPosition, Vector2.zero, 0, characterLayer);
+			
+			if (hit.collider != null) {
+				if(hit.collider == myCollider) {
+					playerManager.SetCharacterType(myCharacter);
+					
+					canMove = true;
+					positionOffset = startPosition - (Vector2)transform.position;
+				}
+			}
+		}
+		
+		if(gesture.Phase == ContinuousGesturePhase.Updated && canMove) {
+			transform.position = (Vector2)Camera.main.ScreenToWorldPoint(gesture.Position) + positionOffset;
+			moving = true;
+		}
+		
+		if(gesture.Phase == ContinuousGesturePhase.Ended) {
+			canMove = false;
+			moving = false;
+		}
+	}
 }
